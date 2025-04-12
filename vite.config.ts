a7f9c46df72be81279e18bbebefc0c92f2a3d3fd
Divyanshu <dk5506934@gmail.com>
2025-04-12 23:59:58 +0530

    Resolve merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -7,14 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-<<<<<<< HEAD
-    host: "::",
-    port: 5173,
-=======
     host: "localhost",
-    port: 8080,
+    port: 5173,
     strictPort: false,
->>>>>>> 30cbc90a36c5bae970d7666e52168830c8271ba1
   },
   plugins: [
     react(),
